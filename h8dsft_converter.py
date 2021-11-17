4c3c9a2bd837bd680c4ea6781418e56ec931d138
dbace23 <d_bace23@hotmail.com>
2021-11-17 19:22:57 +0700

    revisi formated string di return

--- h8dsft_converter.py
+++ h8dsft_converter.py
@@ -14,11 +14,8 @@ def ubahK2C(angka=0):
 ###cek kondisi dengan else apabila kondisi mode tidak dipenuhi akan end
 def ubahTemp(angka2=0,mode="farenheit"):
   if mode.lower()=="celcius":
-    print("hasil konversi dari", angka2,"celcius ke farenheit adalah")
-    return ubahC2K(angka=angka2)
+    return f"hasil konversi dari {angka2} celcius adalah {ubahC2K(angka=angka2)} farenheit "
   elif mode.lower()=="farenheit":
-    print("hasil konversi dari", angka2,"farenheit ke celcius adalah")
-    return ubahK2C(angka=angka2)
-  else:
+    return f"hasil konversi dari {angka2} farenheit adalah {ubahK2C(angka=angka2)} celcius" 
     print("masukin celcius/ farenheit")
 ubahTemp(mode="farenheit")
